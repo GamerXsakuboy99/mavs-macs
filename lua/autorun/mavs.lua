64144a0164a0da6770be491fd0656e4332336a68
GamerzXsakuzboy99 <GamerXsakuboy99@users.noreply.github.com>
2024-05-29 05:45:21 +0300

    first version, Not ready yet :D

---
+++ lua/autorun/mavs.lua
@@ -0,0 +1,64 @@
+--[[
+
+###### METROSTROI AUTOMATIC VOLTAGE SETTER [MAVS] ########
+Coded by GamerzXsakuzboy99
+Primary use: Setting Voltage automaticlly per-map.
+
+Copyrighted under CC BY-NC-ND 4.0 & https://creativecommons.org/licenses/by-nc-nd/4.0/
+
+--]]
+timer.Simple(0.5, function()
+  if not Metrostroi then return end
+  MsgC(Color(255, 0, 0), "[MAVS] Scaning for the current map to set the Voltage.")
+  MsgC(Color(120, 220, 100, 255), "This script has been made for Metrostroi Europe server and relased later for public usage.")
+  MsgC(Color(120, 220, 100, 255), "This script is only downloadble from GamerzXsakuzboy99's GitHub account https://github.com/GamerXsakuboy99")
+  MsgC(Color(120, 220, 100, 255), "Releasing this script to workshop & updating outside of GitHub Repostiory is not allowed, Please push updates via Pull requests :)")
+  MsgC(Color(120, 220, 100, 255), "- GamerzXsakuzboy99")
+  local map = game.GetMap() or ""
+
+  local VoltageTbl = {
+    ["gm_dnipro"] = 750,
+    ["gm_jar_pll_remastered_v12"] = 820,
+    ["gm_metro_jar_imagine_line_v4"] = 820,
+    ["gm_metro_chapaevskaya_line_redux"] = 820,
+    ["gm_metro_kalinin_v3"] = 820,
+    ["gm_metro_nekrasovskaya_line_v5"] = 820,
+    ["gm_metro_m3_new"] = 820,
+    ["gm_budapest_m5"] = 820,
+    ["gm_metro_minsk_1984"] = 820,
+    ["gm_metro_mosldl_v1"] = 820,
+    ["gm_metro_ndr_val_v2r1"] = 820,
+    ["gm_metro_nsk_line_2_v6"] = 820,
+    ["gm_metro_ruralline_v29"] = 800,
+    ["gm_metro_ruralline_v29_old"] = 820,
+    ["gm_metro_ruralline_v3"] = 800,
+    ["gm_metro_ruralline_v4"] = 850,
+    ["gm_metro_ruralline_v29_snow"] = 830,
+    ["gm_metro_platform_doors_v1"] = 820,
+    ["gm_metro_sunnytown_v2"] = 800,
+    ["gm_metro_lichterfelde"] = 800,
+    ["gm_metro_lichterfelde_nomirros"] = 800,
+    ["gm_metro_blue_red_lines_v1"] = 820,
+    ["gm_metro_u5"] = 820,
+    ["gm_metro_u6"] = 820,
+    ["gm_smr_1987"] = 820,
+    ["gm_smr_first_line_v3"] = 800,
+    ["gm_mus_loopline_e"] = 820,
+    ["gm_mus_neoorange_e"] = 820,
+    ["gm_mustox_neocrimson_line_a"] = 820,
+    ["gm_metrostroi_b50"] = 820,
+    ["gm_metronvl"] = 820,
+    ["gm_metro_virus"] = 820,
+    ["gm_metro_surfacemetro_w"] = 820,
+    ["gm_metro_krl_v1"] = 820,
+    ["gm_metro_prizmaaline_v3"] = 820,
+    ["gm_metro_crossline_c4"] = 820,
+    ["gm_metro_crossline_r199h"] = 820,
+    ["gm_metro_crossline_n4a"] = 820
+  } --Add new maps inside {}
+
+  if not VoltageTbl[map] then return end
+  local voltage = VoltageTbl[map]
+  print("Found " .. (map or "invalid map, Using Default voltage.") .. ", Setting wagon limit to " .. VoltageTbl[map])
+  RunConsoleCommand("metrostroi_voltage", voltage)
+end)
---
+++ readme.md
@@ -0,0 +1,20 @@
+# METROSTROI AUTOMATIC VOLTAGE SETTER [MAVS] & METROSTROI AUTOMATIC CURRENT SETTER [MACS]
+###### Working title :D
+
+## What is MAVS and MACS?
+MAVS and MACS is automated script what sets Voltage and Current limits accordingly to the map(s).
+This script changes **metrostroi_current_limit** & **metrostroi_voltage** parameters.
+
+As this is my first codding, the code might look very messy and not that nice to watch.
+But i'm planning to rework the code later when i learn more of the coding :D
+
+### Copyright / etc
+Copyrighted under [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/)
+- **Share** = copy and redistribute the material in any medium or format
+- **Attribution** = You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use. 
+- **NonCommercial** =  You may not use the material for commercial purposes.
+- **NoDerivatives** = If you remix, transform, or build upon the material, you may not distribute the modified material.
+    - Releasing this script to workshop & updating outside of GitHub Repository is not allowed, Please push updates via Pull requests :)
+- This script is only downloadable from GamerzXsakuzboy99's GitHub account https://github.com/GamerXsakuboy99
+
+
